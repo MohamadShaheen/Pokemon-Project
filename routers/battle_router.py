9c9e5ba12548fc19ba66039000c75f985815d8ab
Adam <akaabyia@yahoo.com>
2024-06-25 16:36:49 +0300

    battle added and working with docker

--- routers/battle_router.py
+++ routers/battle_router.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter, HTTPException
 
 from DAL.trainer_battle_interactor import TrainerBattle
@@ -8,14 +10,11 @@ router = APIRouter()
 
 @router.put('/')
 async def Battle(trainer1_name, trainer2_name):
-    pokemons_interactor=PokemonsInteractor()
+    pokemons_interactor = PokemonsInteractor()
     trainer1_pokemons = pokemons_interactor.get_pokemons_by_trainer(trainer1_name)
-    return "hey"
 
     trainer2_pokemons = pokemons_interactor.get_pokemons_by_trainer(trainer2_name)
 
-    battle = TrainerBattle(trainer1_name,
-                           trainer2_name,
-                           trainer1_pokemons,
-                           trainer2_pokemons)
+    battle = TrainerBattle(trainer1_name, trainer2_name, trainer1_pokemons, trainer2_pokemons)
+    #print(json.dumps(battle.simulate_battle(), indent=4))
     return battle.simulate_battle()
